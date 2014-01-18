9ea9a8c64da24c91d0b92e7af39d27180d69f150
Tad Thorley <phaedryx@gmail.com>
2014-01-17 23:03:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReactRailsBackbone::Application.config.secret_key_base = '8cd2a66e93d7444e1d853f203c35031c0a4f3530e799b5c3ef48aa616ce37fd0f6de3f3c6b0dc13c939fe22ddb597f0c3d8721ce32c3934b034e84066278c52b'
